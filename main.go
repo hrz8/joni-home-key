af996bf9737f380a8ea9d4fcb9201ad108c75ff5
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2020-10-23 23:18:54 +0700

    transform method as receiver method

--- main.go
+++ main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 type (
+	// PositionInterface is represent methods of Position struct
+	PositionInterface interface {
+		goingNorth() bool
+		goingEast() bool
+		goingSouth() bool
+	}
+
 	// Position is represent coordinate
 	Position struct {
 		Row int
@@ -16,6 +23,57 @@ type (
 		South int
 	}
 )
+
+func (p *Position) goingNorth() bool {
+	x := &p.Row
+	incrementNorth := -1
+	maxNorth := 0
+	*x += incrementNorth
+	return *x > maxNorth
+}
+
+func (p *Position) goingEast() bool {
+	y := &p.Col
+	incrementEast := 1
+	maxEast := 7
+	*y += incrementEast
+	return *y < maxEast
+}
+
+func (p *Position) goingSouth() bool {
+	x := &p.Row
+	incrementSouth := 1
+	maxSouth := 5
+	*x += incrementSouth
+	return *x < maxSouth
+}
+
+func (p Position) findKey(f *[][]byte) {
+	posNorth := p
+	for posNorth.goingNorth() {
+		if (*f)[posNorth.Row][posNorth.Col] == 0 {
+			break
+		}
+		fmt.Println(&posNorth)
+
+		posEast := posNorth
+		for posEast.goingEast() {
+			if (*f)[posEast.Row][posEast.Col] == 0 {
+				break
+			}
+			fmt.Println(&posEast)
+
+			posSouth := posEast
+			for posSouth.goingSouth() {
+				if (*f)[posSouth.Row][posSouth.Col] == 0 {
+					break
+				}
+				fmt.Println(&posSouth)
+				fmt.Println("gaca!")
+			}
+		}
+	}
+}
 
 func main() {
 	floor := [][]byte{
@@ -32,58 +90,7 @@ func main() {
 		Col: 1,
 	}
 
-	findKey(joniPos, &floor)
+	joniPos.findKey(&floor)
 
 	fmt.Println(joniPos)
-}
-
-func goingNorth(p *Position) bool {
-	x := &p.Row
-	incrementNorth := -1
-	maxNorth := 0
-	*x += incrementNorth
-	return *x > maxNorth
-}
-
-func goingEast(p *Position) bool {
-	y := &p.Col
-	incrementEast := 1
-	maxEast := 7
-	*y += incrementEast
-	return *y < maxEast
-}
-
-func goingSouth(p *Position) bool {
-	x := &p.Row
-	incrementSouth := 1
-	maxSouth := 5
-	*x += incrementSouth
-	return *x < maxSouth
-}
-
-func findKey(p Position, f *[][]byte) {
-	posNorth := p
-	for goingNorth(&posNorth) {
-		if (*f)[posNorth.Row][posNorth.Col] == 0 {
-			break
-		}
-		fmt.Println(&posNorth)
-
-		posEast := posNorth
-		for goingEast(&posEast) {
-			if (*f)[posEast.Row][posEast.Col] == 0 {
-				break
-			}
-			fmt.Println(&posEast)
-
-			posSouth := posEast
-			for goingSouth(&posSouth) {
-				if (*f)[posSouth.Row][posSouth.Col] == 0 {
-					break
-				}
-				fmt.Println(&posSouth)
-				fmt.Println("gaca!")
-			}
-		}
-	}
 }
