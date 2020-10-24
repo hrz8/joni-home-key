e65d562c6557ad8c35bbed724329562a099fafd9
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2020-10-24 21:39:05 +0700

    add recursive function

--- lib/lib.go
+++ lib/lib.go
@@ -11,7 +11,10 @@ type (
 		GoingNorth(f *[][]byte) bool
 		GoingEast(f *[][]byte) bool
 		GoingSouth(f *[][]byte) bool
-		FindKey(f *[][]byte) ([]Step, error)
+		GoingWest(f *[][]byte) bool
+		Walking(r *[]Step, f *[][]byte, d []string, i int, n *int, e *int, s *int, w *int) error
+		FindPosibilities(f *[][]byte, d []string) ([]Step, error)
+		FindPosibilitiesV1(f *[][]byte) ([]Step, error)
 	}
 
 	// Position is represent coordinate
@@ -26,38 +29,131 @@ type (
 		North int
 		East  int
 		South int
+		West  int
 	}
 )
 
 // GoingNorth is method to let Position walking through north untill reach the edge
 func (p *Position) GoingNorth(f *[][]byte) bool {
-	x := &p.Row
+	y := &p.Row
 	incrementNorth := -1
 	maxNorth := 0
-	*x += incrementNorth
-	return *x > maxNorth
+	*y += incrementNorth
+	return *y > maxNorth
 }
 
 // GoingEast is method to let Position walking through east untill reach the edge
 func (p *Position) GoingEast(f *[][]byte) bool {
-	y := &p.Col
+	x := &p.Col
 	incrementEast := 1
 	maxEast := len((*f)[0]) - 1
-	*y += incrementEast
-	return *y < maxEast
+	*x += incrementEast
+	return *x < maxEast
 }
 
 // GoingSouth is method to let Position walking through south untill reach the edge
 func (p *Position) GoingSouth(f *[][]byte) bool {
-	x := &p.Row
+	y := &p.Row
 	incrementSouth := 1
 	maxSouth := len(*f) - 1
-	*x += incrementSouth
-	return *x < maxSouth
+	*y += incrementSouth
+	return *y < maxSouth
 }
 
-// FindKey is method to let Position find the key from North -> East -> South move in the given floor
-func (p Position) FindKey(f *[][]byte) ([]Step, error) {
+// GoingWest is method to let Position walking through south untill reach the edge
+func (p *Position) GoingWest(f *[][]byte) bool {
+	x := &p.Col
+	incrementWest := -1
+	maxWest := 0
+	*x += incrementWest
+	return *x > maxWest
+}
+
+// Walking is method to let Position find the key from move in the given floor
+func (p Position) Walking(r *[]Step, f *[][]byte, d []string, i int, n *int, e *int, s *int, w *int) error {
+	var err error
+	cp := p
+
+	for {
+		dc := d[i]
+		var going bool
+
+		switch dc {
+		case "north":
+			going = cp.GoingNorth(f)
+			*n = int(math.Abs(float64(p.Row) - float64(cp.Row)))
+		case "east":
+			going = cp.GoingEast(f)
+			*e = int(math.Abs(float64(p.Col) - float64(cp.Col)))
+		case "south":
+			going = cp.GoingSouth(f)
+			*s = int(math.Abs(float64(p.Row) - float64(cp.Row)))
+		case "west":
+			going = cp.GoingWest(f)
+			*w = int(math.Abs(float64(p.Col) - float64(cp.Col)))
+		default:
+			err = errors.New("unknown direction clue")
+			if err != nil {
+				return err
+			}
+		}
+
+		if going == false || (*f)[cp.Row][cp.Col] == 0 {
+			break
+		}
+
+		if dc == d[len(d)-1] {
+			*r = append(*r, Step{
+				Position: Position{
+					Row: cp.Row,
+					Col: cp.Col,
+				},
+				North: *n,
+				East:  *e,
+				South: *s,
+				West:  *w,
+			})
+		} else {
+			np := cp
+			ni := i + 1
+
+			err = np.Walking(r, f, d, ni, n, e, s, w)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// FindPosibilities is method to let Position find the key from anywhere in the given floor
+func (p Position) FindPosibilities(f *[][]byte, d []string) ([]Step, error) {
+	colSize := len((*f)[0])
+	for _, col := range *f {
+		if len(col) != colSize {
+			err := errors.New("floor size not sync")
+			return nil, err
+		}
+	}
+
+	initalIndex := 0
+	result := make([]Step, initalIndex)
+
+	sn := 0 // step to north
+	se := 0 // step to east
+	ss := 0 // step to south
+	sw := 0 // step to west
+
+	if err := p.Walking(&result, f, d, initalIndex, &sn, &se, &ss, &sw); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// FindPosibilitiesV1 is method to let Position find the key from North -> East -> South move in the given floor
+func (p Position) FindPosibilitiesV1(f *[][]byte) ([]Step, error) {
 	colSize := len((*f)[0])
 	for _, col := range *f {
 		if len(col) != colSize {
--- main.go
+++ main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hrz8/joni-home-key/lib"
 )
 
 func main() {
+	floor := &[][]byte{
+		// #1 -> 0 is wall, unavailable route
+		// #2 -> everything not 0 is available route
+
+		/* col
+		|0, 1, 2, 3, 4, 5, 6, 7| */
+		{0, 0, 0, 0, 0, 0, 0, 0}, // row 0
+		{0, 1, 1, 1, 1, 1, 1, 0}, // row 1
+		{0, 1, 0, 0, 0, 1, 1, 0}, // row 2
+		{0, 1, 1, 1, 0, 1, 0, 0}, // row 3
+		{0, 1, 0, 1, 1, 1, 1, 0}, // row 4
+		{0, 0, 0, 0, 0, 0, 0, 0}, // row 5
+	}
+
+	clue := []string{
+		"north", // 1sr step
+		"east",  // 2nd step
+		"south", // 3rd step
+	}
+
 	joniPosition := lib.NewPositionInterface(4, 1)
-	posibleKeyPositions, err := joniPosition.FindKey(&[][]byte{
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 1, 1, 1, 1, 1, 1, 0},
-		{0, 1, 0, 0, 0, 1, 1, 0},
-		{0, 1, 1, 1, 0, 1, 0, 0},
-		{0, 1, 0, 1, 1, 1, 1, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-	})
+	posibleKeyPositions, err := joniPosition.FindPosibilities(floor, clue)
+	// posibleKeyPositions, err := joniPosition.FindPosibilitiesV1(floor)
 	if err != nil {
 		panic(fmt.Sprintf("[ERROR] %s", err.Error()))
 	}
 
-	fmt.Println("These are posible key position coordinates (row, col) with moves (north -> east -> south):")
+	fmt.Printf("These are posible key position coordinates (row, col) with moves %s:\n", strings.Join(clue, "->"))
 	if len(posibleKeyPositions) > 0 {
 		for i, keyPos := range posibleKeyPositions {
 			fmt.Printf("%d. at (%d, %d) with move (%d -> %d -> %d)\n", i+1, keyPos.Position.Row, keyPos.Position.Col, keyPos.North, keyPos.East, keyPos.South)
@@ -28,5 +43,4 @@ func main() {
 	} else {
 		fmt.Println("0. no posible position!")
 	}
-
 }
